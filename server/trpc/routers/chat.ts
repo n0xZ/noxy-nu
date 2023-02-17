153d9dc74e7d7639105494bd8a56bb3fd332b25e
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-02-17 15:09:14 -0300

    chore: add chatBasedOnUserInfo to get user-> based on email-> and fetch chats based on that user info

--- server/trpc/routers/chat.ts
+++ server/trpc/routers/chat.ts
@@ -12,6 +12,21 @@ export const chatRouter = router({
 			const chats = await ctx.chat.findFirst({ where: { userId: input.userId } })
 			return chats
 		}),
+	chatsBasedOnUserInfo: publicProcedure
+		.input(
+			z.object({
+				email: z.string(),
+			})
+		)
+		.query(async ({ input, ctx }) => {
+			const existingUser = await ctx.user.findFirst({
+				where: { email: input.email },
+			})
+			const existingUserChats = await ctx.chat.findMany({
+				where: { userId: existingUser?.id },
+			})
+			return existingUserChats
+		}),
 	chatById: publicProcedure
 		.input(z.object({ id: z.string() }))
 		.query(async ({ input, ctx }) => {
@@ -22,6 +37,12 @@ export const chatRouter = router({
 		.input(z.object({ userId: z.string() }))
 		.mutation(async ({ ctx, input }) => {
 			const createdChat = await ctx.chat.create({ data: { userId: input.userId } })
+			return createdChat
+		}),
+	deleteChat: publicProcedure
+		.input(z.object({ id: z.string() }))
+		.mutation(async ({ ctx, input }) => {
+			const createdChat = await ctx.chat.delete({ where: { id: input.id } })
 			return createdChat
 		}),
 })
