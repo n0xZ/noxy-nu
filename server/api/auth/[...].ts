f645c54e5ae926566233f9f6d901408c82673c25
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-02-16 21:12:44 -0300

    setup nuxt-auth

---
+++ server/api/auth/[...].ts
@@ -0,0 +1,17 @@
+import { NuxtAuthHandler } from '#auth'
+import GithubProvider from 'next-auth/providers/github'
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import { client } from '~/utils/prisma'
+const CLIENT_ID = process.env.CLIENT_ID!
+const CLIENT_SECRET = process.env.CLIENT_SECRET!
+
+export default NuxtAuthHandler({
+	adapter: PrismaAdapter(client),
+	providers: [
+		// @ts-expect-error
+		GithubProvider.default({
+			clientId: CLIENT_ID,
+			clientSecret: CLIENT_SECRET,
+		}),
+	],
+})
