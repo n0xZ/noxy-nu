6b2fd0e530d1191e5424cd252240a12958ba20d4
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-02-18 16:14:56 -0300

    chore: add chat list

---
+++ components/chat/list.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+	import { Chat } from '@prisma/client'
+
+	type Props = {
+		chats: Chat[]
+	}
+	const props = defineProps<Props>()
+</script>
+
+<template>
+	<article class="w-full flex flex-col justify-center mt-4 gap-6">
+		<ChatItem v-for="chat in props.chats" :chat="chat" />
+	</article>
+</template>
