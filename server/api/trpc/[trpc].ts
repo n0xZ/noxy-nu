87be66733be165ab8cafebdce8caee9af5868cfe
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-02-18 18:48:34 -0300

    chore: add message router

--- server/api/trpc/[trpc].ts
+++ server/api/trpc/[trpc].ts
@@ -2,9 +2,10 @@ import { createNuxtApiHandler } from 'trpc-nuxt'
 import { mergeRouters } from '~/server/trpc/trpc'
 import { chatRouter } from '~/server/trpc/routers/chat'
 import { userRouter } from '~/server/trpc/routers/user'
+import { messageRouter } from '~~/server/trpc/routers/message'
 import { createContext } from '~~/server/trpc/context'
 
-export const appRouter = mergeRouters(chatRouter, userRouter)
+export const appRouter = mergeRouters(chatRouter, userRouter, messageRouter)
 
 export type AppRouter = typeof appRouter
 
---
+++ server/trpc/routers/message.ts
@@ -0,0 +1,29 @@
+import { publicProcedure, router } from '../trpc'
+import { z } from 'zod'
+
+export const messageRouter = router({
+	messagesByChatId: publicProcedure
+		.input(z.object({ chatId: z.string() }))
+		.query(async ({ input: { chatId }, ctx }) => {
+			const messages = await ctx.message.findMany({ where: { id: chatId } })
+			return messages
+		}),
+	createMessage: publicProcedure
+		.input(
+			z.object({ chatId: z.string(), authorId: z.string(), content: z.string() })
+		)
+		.query(async ({ input: { chatId, authorId, content }, ctx }) => {
+			const createdMessage = await ctx.message.create({
+				data: { authorId, content, chatId },
+			})
+			return createdMessage
+		}),
+	deleteMessage: publicProcedure
+		.input(z.object({ id: z.string() }))
+		.query(async ({ input: { id }, ctx }) => {
+			const deletedMessage = await ctx.message.delete({
+				where: { id },
+			})
+			return deletedMessage
+		}),
+})
