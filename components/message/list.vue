e5080dbe21731fa374ea6688e57edfa3463d16bc
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-02-19 17:42:31 -0300

    chore: remove messages props and set slot

--- components/message/list.vue
+++ components/message/list.vue
@@ -1,21 +1,8 @@
-<script setup lang="ts">
-	import { Message } from '@prisma/client'
-
-	type Props = {
-		messages: Message[]
-	}
-	defineProps<Props>()
-</script>
-
 <template>
 	<ul
-		class="max-h-screen h-screen  w-full overflow-y-scroll flex flex-col-reverse p-2 gap-5 mb-10 relative"
+		class="max-h-screen h-screen w-full overflow-y-scroll flex flex-col-reverse p-2 gap-5 mb-10 relative"
 		v-auto-animate
 	>
-		<MessageItem
-			v-for="message in messages"
-			:key="message.id"
-			:message="message"
-		/>
+		<slot />
 	</ul>
 </template>
