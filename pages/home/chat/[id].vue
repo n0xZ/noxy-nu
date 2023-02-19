f4779524317f676eb8c28a81babfc2e58b58d082
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-02-19 16:00:36 -0300

    set dynamic page to handle fetchs based on id

--- pages/home/chat/[id].vue
+++ pages/home/chat/[id].vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
+	type MessageStatus = 'LOADING' | 'ERROR' | 'SENDED' | 'STALE'
 	const route = useRoute()
 
 	const { $client } = useNuxtApp()
-	const chatId = String(route.query.id).trim()
-	const { data: messages, pending, refresh } = useGetMessageByChatId(chatId)
+	const chatId = String(route.params.id)
+	const { data: messages, pending } = useGetMessageByChatId(chatId)
+	const messageStatus = ref<MessageStatus>('STALE')
 	const sendMessage = async ({
 		authorId,
 		chatId,
@@ -18,23 +20,25 @@
 			chatId,
 			content,
 		})
-		if (newMessage) messages.value?.unshift(newMessage)
+		messageStatus.value = 'LOADING'
+		if (!newMessage) throw createError('An error ocurred while sending message.')
+		messages.value?.unshift(newMessage)
+		messageStatus.value = 'SENDED'
 	}
-	onBeforeRouteUpdate((to, from) => {
-		if (to.query.id !== from.query.id) refresh()
-	})
 </script>
 
 <template>
 	<aside class="p-5 border-b-2 border-neutral-50 block w-full">
-		<ul class="flex flex-row items-center justify-between container mx-auto">
+		<ul
+			class="flex flex-row items-center justify-between container mx-auto block"
+		>
 			<li>
 				<h3 class="font-bold xl:text-2xl">{{ route.query.title }}</h3>
 			</li>
 			<li><MenuEditDropdown /></li>
 		</ul>
 	</aside>
-	<div class="flex flex-col justify-between ml-4">
+	<div class="flex flex-col justify-between">
 		<MessageListSkeleton v-if="pending" />
 		<div
 			class="grid place-items-center h-screen"
@@ -42,10 +46,7 @@
 		>
 			<p>No hay mensajes en este chat</p>
 		</div>
-		<MessageList
-			v-if="messages && !pending && messages.length !== 0"
-			:messages="messages"
-		/>
+		<MessageList v-if="messages && !pending" :messages="messages" />
 		<MessageForm :send-message="sendMessage" />
 	</div>
 </template>
