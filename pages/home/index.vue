7010ec2118ada0b04106fecee7d7cf2c95f36d9c
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-02-18 23:10:50 -0300

    chore: add initial home skeleton

---
+++ pages/home/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+	useHead({ title: 'Noxy-nu | Home' })
+
+</script>
+
+<template>
+	<section class="h-full w-full mt-4">
+		<h2>Bienvenido a Nu!</h2>
+		<p>Este es el panel inicial del chat.</p>
+	</section>
+</template>
